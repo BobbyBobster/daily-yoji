509f8449221a302eca0eb9090f77fb9f247a0e59
Bastiaan Boland <bastiaan.boland@gmail.com>
2026-06-23 11:51:37 +0900

    linting with black

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,18 @@ from sqlalchemy import create_engine
 
 engine = create_engine("sqlite:///yoji.db", pool_pre_ping=True, echo=True)
 
+
 def create_app():
     app = Flask(__name__)
     app.json.ensure_ascii = False
     CORS(app)
-    
+
     from app.web import bp as web_bp
-    app.register_blueprint(web_bp, url_prefix='/')
+
+    app.register_blueprint(web_bp, url_prefix="/")
 
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
--- app/api/routes.py
+++ app/api/routes.py
@@ -5,50 +5,58 @@ from app.api import bp
 from app import engine
 
 
-@bp.route('/')
-@bp.route('/<int:max_id>')
+@bp.route("/")
+@bp.route("/<int:max_id>")
 def index(max_id=2):
     with engine.connect() as conn:
-        query = text('''
+        query = text(
+            """
             SELECT id, kanji FROM yojijukugo
-            WHERE id < :max_id;''')
-        result = conn.execute(query, {'max_id': max_id})
+            WHERE id < :max_id;"""
+        )
+        result = conn.execute(query, {"max_id": max_id})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(('id', 'kanji'), data[0]))
-            ), 200
+    return jsonify(dict(zip(("id", "kanji"), data[0]))), 200
 
 
-@bp.route('/yoji/<int:id_>')
+@bp.route("/yoji/<int:id_>")
 def yoji(id_):
     with engine.connect() as conn:
-        cols = ('id', 'kanji', 'reading', 'usage', 'meaning', 'is_wikipedia_link', 'sentence_id')
-        query = text(f'''
+        cols = (
+            "id",
+            "kanji",
+            "reading",
+            "usage",
+            "meaning",
+            "is_wikipedia_link",
+            "sentence_id",
+        )
+        query = text(
+            f"""
             SELECT {', '.join(cols)} FROM yojijukugo
-            WHERE id = :id;''')
-        result = conn.execute(query, {'id': id_})
+            WHERE id = :id;"""
+        )
+        result = conn.execute(query, {"id": id_})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(cols, data[0]))
-            ), 200
+    return jsonify(dict(zip(cols, data[0]))), 200
 
 
-@bp.route('/yoji/<string:q>')
+@bp.route("/yoji/<string:q>")
 def search_yoji(q):
     with engine.connect() as conn:
-        query = text('''
+        query = text(
+            """
             SELECT id, kanji FROM yojijukugo
-            WHERE kanji = :q;''')
-        result = conn.execute(query, {'q': q})
+            WHERE kanji = :q;"""
+        )
+        result = conn.execute(query, {"q": q})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(('id', 'kanji'), data[0]))
-            ), 200
-        
+    return jsonify(dict(zip(("id", "kanji"), data[0]))), 200
 
-@bp.route('/yoji')
+
+@bp.route("/yoji")
 def search_yoji_with_param():
-    q = request.args.get('q')
+    q = request.args.get("q")
     if q is None:
         abort(400, description='Missing required query parameter "q"')
 
@@ -56,54 +64,63 @@ def search_yoji_with_param():
     return search_yoji(q)
 
 
-@bp.route('/date/<string:date>')
+@bp.route("/date/<string:date>")
 def date_string(date):
     try:
         datetime.date.fromisoformat(date)
     except ValueError:
         return False
 
-    cols = ('id', 'kanji', 'reading', 'usage', 'meaning', 'is_wikipedia_link', 'sentence_id', 'date')
+    cols = (
+        "id",
+        "kanji",
+        "reading",
+        "usage",
+        "meaning",
+        "is_wikipedia_link",
+        "sentence_id",
+        "date",
+    )
 
     with engine.connect() as conn:
-        query = text(f'''
+        query = text(
+            f"""
             SELECT {', '.join(cols)} FROM yojijukugo
-            WHERE date = :date;''')
-        result = conn.execute(query, {'date': date})
+            WHERE date = :date;"""
+        )
+        result = conn.execute(query, {"date": date})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(cols, data[0]))
-            ), 200
+    return jsonify(dict(zip(cols, data[0]))), 200
 
-@bp.route('/date/<int:year>/<int:month>/<int:day>')
+
+@bp.route("/date/<int:year>/<int:month>/<int:day>")
 def date_url(year, month, day):
     return date_string(str(datetime.date(year, month, day)))
 
 
-@bp.route('/sentence/<int:id_>')
+@bp.route("/sentence/<int:id_>")
 def sentence(id_):
-    cols = ('id', 'sentence_text', 'owner', 'translation_id')
+    cols = ("id", "sentence_text", "owner", "translation_id")
 
     with engine.connect() as conn:
-        query = text(f'''
+        query = text(
+            f"""
             SELECT {', '.join(cols)} FROM sentence
-            WHERE id = :id;''')
-        result = conn.execute(query, {'id': id_})
+            WHERE id = :id;"""
+        )
+        result = conn.execute(query, {"id": id_})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(cols, data[0]))
-            ), 200
+    return jsonify(dict(zip(cols, data[0]))), 200
 
 
-@bp.route('/translation/<int:id_>')
+@bp.route("/translation/<int:id_>")
 def translation(id_):
     with engine.connect() as conn:
-        query = text('''
+        query = text(
+            """
             SELECT id, translation_text, owner from translation
-            WHERE id = :id;''')
-        result = conn.execute(query, {'id': id_})
+            WHERE id = :id;"""
+        )
+        result = conn.execute(query, {"id": id_})
         data = result.fetchall()
-    return jsonify(
-            dict(zip(('id', 'translation_text', 'owner'), data[0]))
-            ), 200
-
+    return jsonify(dict(zip(("id", "translation_text", "owner"), data[0]))), 200
